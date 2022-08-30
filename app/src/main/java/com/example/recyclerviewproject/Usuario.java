d2168945b1aefa75f1bf124336669e422745fa4d
NICOLI FERREIRA <u21689@academico.cotuca.unicamp.br>
2022-08-30 09:11:10 -0300

    ki sabor 

---
+++ app/src/main/java/com/example/recyclerviewproject/Usuario.java
@@ -0,0 +1,14 @@
+package com.example.recyclerviewproject;
+
+public class Usuario
+{
+    String nome, email;
+
+    public String getNome() { return nome; }
+
+    public void setNome(String nome) { this.nome = nome; }
+
+    public String getEmail() { return email; }
+
+    public void setEmail(String email) { this.email = email; }
+}
